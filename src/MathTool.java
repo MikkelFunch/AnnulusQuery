19f62f142117e64ab60e7a3dc3efe9692f7d9636
Mark Thorhauge <math@schantz.com>
2016-03-22 13:02:23 +0100

    normal distribution

---
+++ src/MathTool.java
@@ -0,0 +1,16 @@
+import org.apache.commons.math3.distribution.NormalDistribution;
+
+
+public class MathTool {
+	private static double my = 3;
+	private static double stdDiv = 1;
+	private static double reductionFactor = my*2;
+	private static NormalDistribution normalDistribution = new NormalDistribution(0, stdDiv); 
+	
+	public static double normalDistribution() {
+		double sample = (normalDistribution.sample() + my)/reductionFactor;
+		if(sample < 0) sample = 0;
+		if(sample > 1) sample = 1;
+		return sample;
+	}
+}
