b38e15b2da8c6c1ce0d5680555a1f1fd7da0f146
Bladtman <sigurt.bladt@gmail.com>
2016-04-15 11:15:04 +0200

    Added sparsevector implementation

---
+++ src/main/java/mmas/serenderp/util/SparseVector.java
@@ -0,0 +1,47 @@
+package main.java.mmas.serenderp.util;
+import java.util.Collection;
+import java.util.HashMap;
+
+public class SparseVector {
+	private int size;
+	private int nextAvailable=0;
+	private HashMap<Integer,Double> vector;
+	
+	public SparseVector(int size) {
+		this.size=size;
+		this.vector = new HashMap<Integer,Double>(size);
+	}
+
+	//add at last available index
+	public void add(double d) {
+		add(nextAvailable,d);
+	}
+
+	//Overloaded add that takes an integer.
+	//Take care if mixing use of the two add functions, as they move the
+	//nextAvailable index.
+	public void add(int i, double d) {
+		if (null == vector.get(i)) {
+			//this may be overkill
+			throw new RuntimeException("vector overwrite not permitted");
+		}
+		if (d == 0) {
+			return;
+		}
+		vector.put(i, d);
+		nextAvailable = i+1;
+	}
+
+	public double get(int i) {
+		if(i >= size) {
+			//this may be overkill
+			throw new RuntimeException("Vector size overrun");
+		}
+		Double d = vector.get(i);
+		return null == d ? 0 : d;
+	}
+
+	public int size() {
+		return nextAvailable;
+	}
+}
