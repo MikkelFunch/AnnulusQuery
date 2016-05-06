98fe34425455b701759d8abe5975df4953a1924a
Mikkel Funch <mikkel.funch@gmail.com>
2016-05-06 11:49:15 +0200

    todos

--- src/Engine.java
+++ src/Engine.java
@@ -51,9 +51,8 @@ public class Engine {
 		init();
 
 		//Pre process
-		//List<SparseVector> vectors = new ArrayList<>();
 
-		Bucket[] buckets = new Bucket[Constants.getDimensions()];
+		Bucket[] buckets = new Bucket[Constants.getDimensions()]; //TODO: Bucket amounts
 		for (int i = 0; i < buckets.length; i++) {
 			buckets[i] = new Bucket();
 		}
@@ -63,16 +62,17 @@ public class Engine {
 		int count = 0;
 		for (SparseVector sv : movies.values()) {
 			for (int i = 0; i < Constants.getNumberOfHashFunctions(); i++) {
-				for (int x = 0; x < Constants.getDimensions(); x++){
+				for (int x = 0; x < Constants.getDimensions(); x++){ //TODO: this ain't right
 					if (sv.get(MinHashing.hash(i, x)) > THRESHOLD) {
 						buckets[x].add(sv);
+						break;
 					}
 				}
 			}
 			count++;
-			Long endTime = System.currentTimeMillis();
-			Long duration = (endTime - startTime);
 			if (count % 500 == 0) {
+				Long endTime = System.currentTimeMillis();
+				Long duration = (endTime - startTime);
 				System.out.println(count + " - Duration: " + (duration / 1000));
 				startTime = System.currentTimeMillis();
 			}
