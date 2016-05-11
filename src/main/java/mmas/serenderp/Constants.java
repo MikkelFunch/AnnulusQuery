cbdd89421c9b97da06fbf4e40d2232e64d8182ea
Bladtman <sigurt.bladt@gmail.com>
2016-05-11 15:19:35 +0200

    MMore magic

--- src/main/java/mmas/serenderp/Constants.java
+++ src/main/java/mmas/serenderp/Constants.java
@@ -5,6 +5,6 @@ public class Constants {
 	public static final int AMOUNT_OF_RANDOM_VECTORS = 4;
 	public static final double R = 20, W = 1.85, C = 1.41;
 	public static final int NUMBER_OF_BANDS = 7;
-	public static final int HASH_FUNCTIONS_PER_BAND = 2;
+	public static final int HASH_FUNCTIONS_PER_BAND = 1;
 	public static final int NUMBER_OF_HASH_FUNCTIONS = NUMBER_OF_BANDS * HASH_FUNCTIONS_PER_BAND;
 }
