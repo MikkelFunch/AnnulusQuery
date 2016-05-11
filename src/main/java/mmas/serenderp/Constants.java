9f6a025de1d041f64fb612d60536d6624b83c3d9
Mikkel Funch <mikkel.funch@gmail.com>
2016-05-11 20:40:26 +0200

    Implemented memory efficient buckets

--- src/main/java/mmas/serenderp/Constants.java
+++ src/main/java/mmas/serenderp/Constants.java
@@ -2,9 +2,9 @@ package main.java.mmas.serenderp;
 
 public class Constants {
 	public static final int DIMENSIONS = 4_080_356;
-	public static final int AMOUNT_OF_RANDOM_VECTORS = 4;
+	public static final int AMOUNT_OF_RANDOM_VECTORS = 1;
 	public static final double R = 20, W = 1.85, C = 1.41;
-	public static final int NUMBER_OF_BANDS = 7;
-	public static final int HASH_FUNCTIONS_PER_BAND = 1;
+	public static final int NUMBER_OF_BANDS = 20;
+	public static final int HASH_FUNCTIONS_PER_BAND = 2;
 	public static final int NUMBER_OF_HASH_FUNCTIONS = NUMBER_OF_BANDS * HASH_FUNCTIONS_PER_BAND;
 }
